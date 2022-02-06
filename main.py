22bce675deb247a7baa906776a93ac2166162ba4
Yuchen Lei <yuchenl@kth.se>
2022-02-06 23:14:27 +0100

    Add ops within two regs and input case insensitive
    updates:
    (1) realize <register_1> <operation> <register_2>
    (2) all input is case insensitive now.

--- main.py
+++ main.py
@@ -5,10 +5,16 @@
 class register:
     def __init__(self):
         self._value = 0
-        self._get_value_flag = True  # when the flag is True, the value can be accessed.
+        self._access_value_flag = True  
+        # when flag is True, the value can be accessed directly; 
+        # when flag is False, the value can be accessed after the operations between different registers
     def get_value(self):
         return self._value
-    def operation(self, value, op_name):
+    def get_flag(self):
+        return self._access_value_flag
+    def set_flag(self,flag):  
+        self._access_value_flag = flag
+    def operation_val(self, value, op_name): # the operation with value
         if op_name == "add":
             self.add(value)
         elif op_name == "subtract":
@@ -36,12 +42,22 @@ class calculator:
         self._reg_op_dict[reg_name] = []
         # print(reg_name)
 
-    # def 
+    def operation_reg(self, reg_val_1, reg_val_2, op_name): # the operation between registers
+        if op_name == "add":
+            result = reg_val_1 + reg_val_2
+        elif op_name == "subtract":
+            result = reg_val_1 - reg_val_2
+        elif op_name == "multiply":
+            result = reg_val_1 * reg_val_2
+        else:
+            print("no operation")
+        return result
         
 
     def read_line(self):
         while True:
             line = input()
+            line = line.lower()
             num_word = len(line.split()) # how many words in one line, e.g. print <register> 2 words
             
             if line == "quit": # when 
@@ -65,38 +81,47 @@ class calculator:
                         self.create_reg(reg_name)
                     # if op in self._op_list and reg_name in self._reg_dict.keys():
                     if op in self._op_list:
-                        self._reg_dict[reg_name].operation(value, op)
+                        self._reg_dict[reg_name].operation_val(value, op)
                     else:
                         print("no operation")
                 else:   
+                    #TODO
                     # <register_1> <operation> <register_2>
                     pass
-                    # op = line.split()[1]
-                    # reg_name_1 = line.split()[0]
-                    # reg_name_2 = line.split()[2]
+                    op = line.split()[1]
+                    reg_name_1 = line.split()[0]
+                    reg_name_2 = line.split()[2]
 
-                    # if reg_name_1 not in self._reg_dict.keys(): # create the register if it doesn't exist
-                    #     self.create_reg(reg_name_1)
-                    # if reg_name_2 not in self._reg_dict.keys(): # create the register if it doesn't exist
-                    #     self.create_reg(reg_name_2)
-                    # if op in self._op_list:
-                    #     self._reg_dict[reg_name].
+                    if reg_name_1 not in self._reg_dict.keys(): # create the register if it doesn't exist
+                        self.create_reg(reg_name_1)
+                    if reg_name_2 not in self._reg_dict.keys(): # create the register if it doesn't exist
+                        self.create_reg(reg_name_2)
+                    if op in self._op_list:
+                        if self._reg_dict[reg_name_1].get_flag(): # 
+                            self._reg_dict[reg_name_1].set_flag(False)
+                        self._reg_op_dict[reg_name_1].append([op,reg_name_2])
 
 
             else: # 
                 print("num_word not right nunber of words")
 
 
-
-        
-        
-        pass
-
     def print_reg(self, reg_name):
-        if self._reg_dict[reg_name]._get_value_flag:
-            print(self._reg_dict[reg_name].get_value())
+        print(self.get_reg_value(reg_name))
+
+
+    def get_reg_value(self, reg_name_1):
+        if self._reg_dict[reg_name_1].get_flag():
+            return self._reg_dict[reg_name_1].get_value()
         else:
-            pass
+            op_reg_arr = self._reg_op_dict[reg_name_1] # get all the operations with other registers, e.g.  {A:[["add","B"],["subtract","C"]]
+            reg_value_1 = self._reg_dict[reg_name_1].get_value()
+            for op_reg in op_reg_arr:
+                op = op_reg[0]
+                reg_name_2 = op_reg[1]
+                reg_value_2 = self.get_reg_value(reg_name_2)
+                reg_value_1 = self.operation_reg(reg_value_1, reg_value_2, op)
+            return reg_value_1
 
 
 def main():

5442f31ab19351f66e863fbc7d99fa0517bd42de
Yuchen Lei <yuchenl@kth.se>
2022-02-07 21:07:24 +0100

    Add logging to console
    Invalid commands will be ignored, but be logged to the console.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Created by Yuchen Lei
 # Created on 2022/2/7
 import sys
+import logging
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.WARNING)
 
 class register:
     def __init__(self):
@@ -22,8 +24,7 @@ class register:
         elif op == "multiply":
             self.multiply(val)
         else:
-            print("no operation")
-
+            logging.warning("operation not supported")
     def add(self, val):
         self._val += val
     def subtract(self, val):
@@ -38,10 +39,11 @@ class calculator:
         self._op_list = ["add", "subtract", "multiply"] # the supported operations 
     
     def create_reg(self,reg_name):
-        self._reg_dict[reg_name] = register()
-        self._reg_op_dict[reg_name] = []
+        self._reg_dict[reg_name] = register() # create register object
+        self._reg_op_dict[reg_name] = [] # array to operations with other registers
 
-    def operation_reg(self, reg_val_1, reg_val_2, op): # the operation between registers
+    def operation_reg(self, reg_val_1, reg_val_2, op): 
+        # the operation between registers
         if op == "add":
             result = reg_val_1 + reg_val_2
         elif op == "subtract":
@@ -49,10 +51,9 @@ class calculator:
         elif op == "multiply":
             result = reg_val_1 * reg_val_2
         else:
-            print("no operation")
+            logging.warning("operation not supported")
         return result
         
-
     def read_line(self, from_file, file_name=None):
         if from_file: # if taking the input from a file
             with open(file_name) as f:
@@ -67,7 +68,7 @@ class calculator:
                 if idx < num_lines: # if not reach the EOF, read a line
                     line = lines[idx]
                     idx += 1
-                else: # if reach the EOF, break
+                else: # if reach the EOF, quit the program
                     break
             else:
                 line = input()
@@ -76,52 +77,75 @@ class calculator:
             
             if num_word == 1:
                 # quit
-                if line == "quit": # when 
-                    break
-        
+                if line == "quit": 
+                    break # quit the program
+                else:
+                    logging.warning("command invalid")
+                    continue
             elif num_word == 2: 
                 # print <register>
                 reg_name = line.split()[1]
                 if line.split()[0] == "print" and reg_name in self._reg_dict.keys():
-                    self.print_reg(reg_name)
+                    try:
+                        self.print_reg(reg_name)
+                    except RecursionError: # if the operations between registers can't be solved, raise RecursionError
+                        logging.warning("Operations between registers are invalid")
+                        continue
                 else:
-                   print("no print")
-        
+                   logging.warning("register not defined")
+                   continue
             elif num_word == 3:
                 if line.split()[2].isdecimal(): # if the 3rd word is a value 
                     # <register> <operation> <value>
                     op = line.split()[1]
                     reg_name = line.split()[0]
-                    val = int(line.split()[2]) # str -> int
-                    if reg_name not in self._reg_dict.keys(): # create the register if it doesn't exist
+                    val = int(line.split()[2]) # str -> int # the calculator only supports integer operations
+                    if reg_name not in self._reg_dict.keys() and not reg_name.isdecimal(): 
+                    # create the register if the reg_name doesn't exist and is not a number.
                         self.create_reg(reg_name)
+                    elif reg_name.isdecimal():
+                        logging.warning("register name consist of only numbers")
+                        continue # invalid command, continue to next loop
                     if op in self._op_list:
                         self._reg_dict[reg_name].operation_val(val, op)
                     else:
-                        print("no operation")
+                        logging.warning("operation not supported")
+                        continue
                 else:   
                     # <register_1> <operation> <register_2>
                     op = line.split()[1]
                     reg_name_1 = line.split()[0]
                     reg_name_2 = line.split()[2]
 
-                    if reg_name_1 not in self._reg_dict.keys(): # create the register if it doesn't exist
+                    if reg_name_1 not in self._reg_dict.keys() and not reg_name_1.isdecimal(): 
+                    # create the register if the reg_name doesn't exist and is not a number.
                         self.create_reg(reg_name_1)
-                    if reg_name_2 not in self._reg_dict.keys(): # create the register if it doesn't exist
+                    elif reg_name_1.isdecimal():
+                        logging.warning("register name consist of only numbers")
+                        continue # invalid command, continue to next loop
+                    if reg_name_2 not in self._reg_dict.keys() and not reg_name_2.isdecimal(): 
+                    # create the register if the reg_name doesn't exist and is not a number.
                         self.create_reg(reg_name_2)
+                    elif reg_name_2.isdecimal():
+                        logging.warning("register name can't consist of only numbers")
+                        continue # invalid command, continue to next loop
                     if op in self._op_list:
                         if self._reg_dict[reg_name_1].get_flag(): # 
                             self._reg_dict[reg_name_1].set_flag(False)
                         self._reg_op_dict[reg_name_1].append([op,reg_name_2])
-
-            else: # 
-                print("num_word not right nunber of words")
+                    else:
+                        logging.warning("operation not supported")
+                        continue
+            else:
+                logging.warning("The number of words is invalid, only suport 1/2/3 word(s)")
+                continue
 
 
     def print_reg(self, reg_name):
         print(self.get_reg_val(reg_name))
 
     def get_reg_val(self, reg_name_1):
+        #
         if self._reg_dict[reg_name_1].get_flag():
             return self._reg_dict[reg_name_1].get_val()
         else:
@@ -137,7 +161,6 @@ class calculator:
 
 def main():
     cal = calculator()
-
     if len(sys.argv) == 2: # load the input from a file
         cal.read_line(from_file=True, file_name=sys.argv[1])
     elif len(sys.argv) == 1: # load the input from the standard input stream
